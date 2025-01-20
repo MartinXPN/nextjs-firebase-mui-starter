b1881c1366037e9f82ccc0eb4a9d7ebd35f6b5b1
MartinXPN <mirakyanmartin@gmail.com>
2025-01-20 16:47:17 +0400

    Use the new Grid2 from MUI

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,5 +1,5 @@
 import {memo} from "react";
-import Grid from "@mui/material/Grid";
+import Grid from "@mui/material/Grid2";
 import Container from "@mui/material/Container";
 import Link from "@/components/Link";
 import SocialLinks from "./SocialLinks";
@@ -10,12 +10,12 @@ function Footer() {
         <footer>
         <Container maxWidth="xl" sx={{padding: 4}}>
             <Grid container justifyContent="center" alignContent="center" alignItems="center" columnSpacing={4} marginBottom={2}>
-                <Grid item><Link href="/teach">Teach</Link></Grid>
-                <Grid item><Link href="/pricing">Pricing</Link></Grid>
-                <Grid item><Link href="/faq">FAQ</Link></Grid>
-                <Grid item><Link href="/about">About</Link></Grid>
-                <Grid item><Link href="/privacy">Privacy Policy</Link></Grid>
-                <Grid item><Link href="/terms">Terms and Conditions</Link></Grid>
+                <Grid><Link href="/teach">Teach</Link></Grid>
+                <Grid><Link href="/pricing">Pricing</Link></Grid>
+                <Grid><Link href="/faq">FAQ</Link></Grid>
+                <Grid><Link href="/about">About</Link></Grid>
+                <Grid><Link href="/privacy">Privacy Policy</Link></Grid>
+                <Grid><Link href="/terms">Terms and Conditions</Link></Grid>
             </Grid>
 
             <SocialLinks fontSize="medium" />
--- src/components/home/SocialLinks.tsx
+++ src/components/home/SocialLinks.tsx
@@ -1,5 +1,5 @@
 import {memo} from "react";
-import Grid from "@mui/material/Grid";
+import Grid from "@mui/material/Grid2";
 import Link from "@/components/Link";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -9,10 +9,10 @@ import YouTubeIcon from "@mui/icons-material/YouTube";
 function SocialLinks({fontSize, htmlColor}: {fontSize: 'inherit' | 'large' | 'medium' | 'small', htmlColor?: string}) {
     return <>
         <Grid container justifyContent="center" alignContent="center" alignItems="center" columnSpacing={2} color="primary">
-            <Grid item><Link href="https://youtube.com/@profound.academy" target="_blank" rel="noopener noreferrer" aria-label="Youtube"><YouTubeIcon fontSize={fontSize} htmlColor={htmlColor} /></Link></Grid>
-            <Grid item><Link href="https://www.instagram.com/profound.academy.inc" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><InstagramIcon fontSize={fontSize} htmlColor={htmlColor} /></Link></Grid>
-            <Grid item><Link href="https://www.facebook.com/profound.academy.inc" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><FacebookIcon fontSize={fontSize} htmlColor={htmlColor} /></Link></Grid>
-            <Grid item><Link href="https://linkedin.com/company/profound-academy-inc" target="_blank" rel="noopener noreferrer" aria-label="Linkedin"><LinkedInIcon fontSize={fontSize} htmlColor={htmlColor} /></Link></Grid>
+            <Grid><Link href="https://youtube.com/@profound.academy" target="_blank" rel="noopener noreferrer" aria-label="Youtube"><YouTubeIcon fontSize={fontSize} htmlColor={htmlColor} /></Link></Grid>
+            <Grid><Link href="https://www.instagram.com/profound.academy.inc" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><InstagramIcon fontSize={fontSize} htmlColor={htmlColor} /></Link></Grid>
+            <Grid><Link href="https://www.facebook.com/profound.academy.inc" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><FacebookIcon fontSize={fontSize} htmlColor={htmlColor} /></Link></Grid>
+            <Grid><Link href="https://linkedin.com/company/profound-academy-inc" target="_blank" rel="noopener noreferrer" aria-label="Linkedin"><LinkedInIcon fontSize={fontSize} htmlColor={htmlColor} /></Link></Grid>
         </Grid>
     </>
 }
